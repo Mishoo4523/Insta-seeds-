67f6e203bceda5b0fc7594b087f828e7ca3aa407
Mishoo4523 <mishomurad@gmail.com>
2022-07-09 19:08:57 +0300

    add json web token

--- backend/routes/authentication.js
+++ backend/routes/authentication.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const mongoose= require('mongoose');
 const UserModel = mongoose.model('UserModel');
 const bcrypt = require('bcryptjs');
+const JWT= require('jsonwebtoken');
+const {JWT_SECRET}=require('../config');
 
 
 router.get("/",(req,res)=>{
@@ -16,15 +18,19 @@ router.post('/login', (req,res)=>{
         return res.status(400).json({error:"one or more mandatory field is empty"});  
     }
     UserModel.findOne({email:email})
-    .then((dbuser)=>{
-        if(!dbUser){ //User nod found
-            return res.status(400).json({error:"user not exist!"});  
+    .then((dbUser)=>{
+        if(!dbUser){ //User not found
+            return res.status(400).json({error:"Invalid credentials!"});  
 
         }
         bcrypt.compare(password,dbUSer.password)
-        .then((didMatch)=>{
+        .then((didMatch)=> {
             if(didMatch){
-                return res.status(201).json({error:"User Logged In successfully"});  
+                //return res.status(201).json({error:"User Logged In successfully"});  
+                // creat and send a token
+                const jwtToken = jwt.sign({ _id:dbUser._id}, JWT_SECRET);
+                res.json({ token: jwtToken });
+
             } else {
                 return res.status(400).json({error:"Invalid credentials!"});  
             }
